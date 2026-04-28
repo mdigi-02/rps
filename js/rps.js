bf2e1768e8b742ff4df7b3098f73167f8bf4402d
Myrsky Leppä <myrsky.digi@gmail.com>
2026-04-28 12:44:38 +0300

    improved version: autoplay, keyboard shortcuts, cleaner js

--- js/rps.js
+++ js/rps.js
@@ -1,4 +1,6 @@
-let score = JSON.parse(localStorage.getItem('score')) || { // Default operator
+// Initialization
+
+const score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
   losses: 0,
   ties: 0
@@ -6,17 +8,67 @@ let score = JSON.parse(localStorage.getItem('score')) || { // Default operator
 
 updateScoreElement();
 
-// Alternatively:
+let isAutoPlaying = false;
+let intervalId;
 
-/*
-if (!score) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
+// Game controls
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('scissors');
+});
+
+document.querySelector('.js-reset-score-button')
+  .addEventListener('click', () => {
+    resetScore();
+});
+
+document.querySelector('.js-auto-play-button')
+  .addEventListener('click', () => {
+    autoPlay();
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'Backspace') {
+    resetScore();
   }
+});
+
+// Game logic
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
 }
-*/
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
@@ -61,11 +113,11 @@ function playGame(playerMove) {
 
   localStorage.setItem('score', JSON.stringify(score));
 
-  document.querySelector('.js-result').innerHTML = `${result}`; // Update result
+  document.querySelector('.js-result').innerText = `${result}`; // Update result
   document.querySelector('.js-moves').innerHTML = `
     You
     <img src="img/${playerMove}-emoji.png" class="move-icon"> 
-    
+
     <img src="img/${computerMove}-emoji.png" class="move-icon">
     Computer
     ` // Update moves
@@ -74,21 +126,33 @@ function playGame(playerMove) {
 
 function updateScoreElement() {
   document.querySelector('.js-score')
-    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+    .innerText = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
-function pickComputerMove() {
-  const randomNumber = Math.random();
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+}
 
-  let computerMove = '';
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
 
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = 'rock';
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = 'paper';
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = 'scissors';
+    document.querySelector('.js-auto-play-button')
+      .innerText = 'Stop Playing';
+    
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+
+    document.querySelector('.js-auto-play-button')
+      .innerText = 'Auto Play';
   }
-
-  return computerMove;
 }
